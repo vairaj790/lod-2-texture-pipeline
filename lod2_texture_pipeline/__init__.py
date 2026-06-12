0ab33dffda3fe0d6634593376e4bca0a6f7c27b5
Vaibhav Rajan <fo37nor@ads.uni-jena.de>
2026-06-12 14:18:24 +0200

    edit init

--- lod2_texture_pipeline/__init__.py
+++ lod2_texture_pipeline/__init__.py
@@ -1,1 +1,9 @@
+import os
+import platform
+
+# Windows can load both libomp.dll and libiomp5md.dll through PyTorch/OpenCV/NumPy.
+# Set this before importing the pipeline modules.
+if platform.system() == "Windows":
+    os.environ.setdefault("KMP_DUPLICATE_LIB_OK", "TRUE")
+
 from .pipeline import main, process_building
